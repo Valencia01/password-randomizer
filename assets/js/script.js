c530a546d4e113e5b48ac5ce391f6ff3d368e9c1
Valencia01 <kvalencia.99440@gmail.com>
2023-04-05 18:32:51 -0700

    Added password length prompt with input validation

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,9 +6,21 @@ function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  passwordText.textContent = password;
 
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+function generatePassword() {
+  
+  var passLen = prompt("Input a number for the length of password. Must be at least 5", "i.e. 8");
+  
+  while (isNaN(passLen) || passLen < 5 || passLen - Math.floor(passLen) !== 0) {
+    passLen = prompt("Invalid input please enter a valid number")
+  }
+  
+  
+  return passLen;
+}
