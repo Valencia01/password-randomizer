e127a7a6363c6398ab10a2cb490afff51ab9adc3
Valencia01 <kvalencia.99440@gmail.com>
2023-04-05 20:43:04 -0700

    Generates a random all lowercase password at the length inputted

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,11 +16,23 @@ generateBtn.addEventListener("click", writePassword);
 function generatePassword() {
   
   var passLen = prompt("Input a number for the length of password. Must be at least 5", "i.e. 8");
-  
+
+  const generatedPass = [];
+
+  var lowerCaseStr = 'abcdefghijklmnopqrstuvwxyz';
+  const lowerCase = lowerCaseStr.split('');
+
+
   while (isNaN(passLen) || passLen < 5 || passLen - Math.floor(passLen) !== 0) {
     passLen = prompt("Invalid input please enter a valid number")
   }
+
   
-  
-  return passLen;
+  for(let i = 0; i < passLen; i++) {
+
+    generatedPass[i] = lowerCase[Math.floor(Math.random() * lowerCase.length)];
+  }
+
+
+  return generatedPass.join("");
 }
